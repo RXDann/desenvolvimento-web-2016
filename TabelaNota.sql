b13af1995046c4a2eeeca065d09d6e62e8c5ec35
Caio820 <caio.lds123@gmail.com>
2016-11-17 23:26:21 -0200

    Alteração SQL tabela de notas
    nova tabela

--- TabelaNota.sql
+++ TabelaNota.sql
@@ -1,10 +1,68 @@
-INSERT INTO nota VALUES ('','2016-11-14', 'Desenvolvimento web2', 'Elton Machado', 'Teste');
-INSERT INTO nota VALUES ('','2016-11-15', 'Computacao Grafica', 'Caio LEandro', 'Resolução da prova');
-INSERT INTO nota VALUES ('','2016-11-16', 'SO', 'Vanessa', 'Trabalho em grupo');
-INSERT INTO nota VALUES ('','2016-11-16', 'Qualidade', 'Lucas', 'Notas importantes');
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 18-Nov-2016 Ã s 02:22
+-- VersÃ£o do servidor: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
-SELECT * FROM nota;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-DELETE FROM nota WHERE ID = '3';
 
-UPDATE nota SET NOMECURSO='Desenvolvimento Web' WHERE ID = '4';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cadastro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `inserenota`
+--
+
+CREATE TABLE `inserenota` (
+  `id_nota` int(11) NOT NULL,
+  `identifica` text NOT NULL,
+  `nota` varchar(200) NOT NULL,
+  `id_grupo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `inserenota`
+--
+ALTER TABLE `inserenota`
+  ADD PRIMARY KEY (`id_nota`),
+  ADD KEY `id_grupo_fk` (`id_grupo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `inserenota`
+--
+ALTER TABLE `inserenota`
+  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `inserenota`
+--
+ALTER TABLE `inserenota`
+  ADD CONSTRAINT `grupo_id_fk` FOREIGN KEY (`id_grupo`) REFERENCES `grupo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
